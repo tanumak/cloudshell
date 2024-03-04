3b17274567d52a7dbfbca40990156cff8c008dc9
cloudshell <cloud@shell>
2024-03-04 02:41:24 +0000

    socat

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ cd $basedir
 
 . functions
 
+portforward_java_app=8080
 portforward_argocd=8081
 portforward_gitea=8082
 portforward_harbor=8083
@@ -21,7 +22,11 @@ harbor_pass=Harbor12345
 harbor_host=registry.harbor.local
 harbor_nodeport=30021
 
+java_app_nodeport=31001
+
 act_runner_dir=act_runner
+
+nodeport_forward_type=socat
 
 # docker
 echo -n "google cloudshell check ... "
@@ -92,6 +97,16 @@ if ! gitea_check_cli; then
 else ok
 fi
 
+# socat
+echo -n "socat check ..."
+if ! socat_check_cli; then
+  ng
+  echo -n "socat install ... "
+  socat_install_cli || fail
+  ok
+else ok
+fi
+
 # argocd port-forward, login
 echo -n "argocd condition check ... "
 kubectl_wait "--for=condition=available deployment/argocd-server" "argocd" || fail
@@ -101,6 +116,7 @@ echo -n "port-foward check for argocd ... "
 if ! kubectl_check_portforward "svc/argocd-server" "$portforward_argocd:80" "argocd"; then
   ng
   echo -n "port-foward for argocd ... "
+  check_listening $portforward_argocd && fail
   kubectl_portforward "svc/argocd-server" "$portforward_argocd:80" "argocd" || fail
   ok
 else ok
@@ -132,50 +148,97 @@ if ! kubectl_check_ns "harbor" || ! argocd_check_app "harbor"; then
 else ok
 fi
 
-# gitea port-forward
-echo -n "gitea condition check ... "
-kubectl_wait_exists "ns" "gitea" "5" || fail
-kubectl_wait_exists "deploy" "gitea" "5" "gitea" || fail
-kubectl_wait "--for=condition=available deployment/gitea" "gitea" || fail
+# minikube ip
+echo -n "get minikube ip ... "
+minikube_ip=$(exec_command_out "minikube ip")
+test -z $minikube_ip && fail
 ok
-echo -n "port-foward check for gitea ... "
-if ! kubectl_check_portforward "svc/gitea-http" "$portforward_gitea:3000" "gitea"; then
-  ng
-  echo -n "port-foward for gitea ... "
-  kubectl_portforward "svc/gitea-http" "$portforward_gitea:3000" "gitea" || fail
+
+# gitea port-forward
+if [ "$nodeport_forward_type" == "socat" ]; then
+  echo -n "port-foward check for gitea ... "
+  if ! socat_check_portforward "$portforward_gitea" "$minikube_ip" "$gitea_nodeport_http"; then
+    ng
+    echo -n "port-foward for gitea ... "
+    check_listening $portforward_gitea && fail
+    socat_portforward "$portforward_gitea" "$minikube_ip" "$gitea_nodeport_http" || fail
+    ok
+  else ok
+  fi
+else
+  echo -n "gitea condition check ... "
+  kubectl_wait_exists "ns" "gitea" "5" || fail
+  kubectl_wait_exists "deploy" "gitea" "5" "gitea" || fail
+  kubectl_wait "--for=condition=available deployment/gitea" "gitea" || fail
   ok
-else ok
+  echo -n "port-foward check for gitea ... "
+  if ! kubectl_check_portforward "svc/gitea-http" "$portforward_gitea:3000" "gitea"; then
+    ng
+    echo -n "port-foward for gitea ... "
+    check_listening $portforward_gitea && fail
+    kubectl_portforward "svc/gitea-http" "$portforward_gitea:3000" "gitea" || fail
+    ok
+  else ok
+  fi
 fi
 
 # harbor port-forward
-echo -n "harbor condition check ... "
-kubectl_wait_exists "ns" "harbor" "5" || fail
-kubectl_wait_exists "deploy" "harbor-core" "5" "harbor" || fail
-kubectl_wait "--for=condition=available deployment/harbor-core" "harbor" || fail
-kubectl_wait "--for=condition=ready pod -l component=nginx" "harbor" || fail
-ok
-echo -n "port-foward check for harbor ... "
-if ! kubectl_check_portforward "svc/harbor" "$portforward_harbor:80" "harbor"; then
-  ng
-  echo -n "port-foward for harbor ... "
-  kubectl_portforward "svc/harbor" "$portforward_harbor:80" "harbor" || fail
+if [ "$nodeport_forward_type" == "socat" ]; then
+  echo -n "port-foward check for harbor ... "
+  if ! socat_check_portforward "$portforward_harbor" "$minikube_ip" "$harbor_nodeport"; then
+    ng
+    echo -n "port-foward for harbor ... "
+    check_listening $portforward_harbor && fail
+    socat_portforward "$portforward_harbor" "$minikube_ip" "$harbor_nodeport" || fail
+    ok
+  else ok
+  fi
+else
+  echo -n "harbor condition check ... "
+  kubectl_wait_exists "ns" "harbor" "5" || fail
+  kubectl_wait_exists "deploy" "harbor-core" "5" "harbor" || fail
+  kubectl_wait "--for=condition=available deployment/harbor-core" "harbor" || fail
+  kubectl_wait "--for=condition=ready pod -l component=nginx" "harbor" || fail
   ok
-else ok
+  echo -n "port-foward check for harbor ... "
+  if ! kubectl_check_portforward "svc/harbor" "$portforward_harbor:80" "harbor"; then
+    ng
+    echo -n "port-foward for harbor ... "
+    check_listening $portforward_harbor && fail
+    kubectl_portforward "svc/harbor" "$portforward_harbor:80" "harbor" || fail
+    ok
+  else ok
+  fi
 fi
 
 # hosts
 echo -n "check hosts ... "
-hosts_check "$gitea_host" || hosts_update "$gitea_host" || fail
-hosts_check "$harbor_host" || hosts_update "$harbor_host" || fail
-hosts_check_minikube "$harbor_host" || hosts_update_minikube "$harbor_host" || fail
+hosts_check "$gitea_host" $minikube_ip || hosts_update "$gitea_host" $minikube_ip || fail
+hosts_check "$harbor_host" $minikube_ip || hosts_update "$harbor_host" $minikube_ip || fail
+hosts_check_minikube "$harbor_host" $minikube_ip || hosts_update_minikube "$harbor_host" $minikube_ip || fail
 ok
 
 # harbor login
+if [ "$nodeport_forward_type" == "socat" ]; then
+  echo -n "harbor condition check ... "
+  kubectl_wait_exists "ns" "harbor" "5" || fail
+  kubectl_wait_exists "deploy" "harbor-core" "5" "harbor" || fail
+  kubectl_wait "--for=condition=available deployment/harbor-core" "harbor" || fail
+  kubectl_wait "--for=condition=ready pod -l component=nginx" "harbor" || fail
+  ok
+fi
 echo -n "login harbor ... "
 harbor_login "http://$harbor_host:$harbor_nodeport/" "$harbor_user" "$harbor_pass" || fail
 ok
 
 # gitea setting
+if [ "$nodeport_forward_type" == "socat" ]; then
+  echo -n "gitea condition check ... "
+  kubectl_wait_exists "ns" "gitea" "5" || fail
+  kubectl_wait_exists "deploy" "gitea" "5" "gitea" || fail
+  kubectl_wait "--for=condition=available deployment/gitea" "gitea" || fail
+  ok
+fi
 for repo in java-app java-app-manifest; do
   echo -n "check gitea repo ($repo) ... "
   if ! gitea_check_repo "$gitea_host:$gitea_nodeport_http" $gitea_user $gitea_pass $repo; then
@@ -306,6 +369,16 @@ if ! argocd_check_app "java-app"; then
 else ok
 fi
 
+echo -n "port-foward check for java-app ... "
+if ! socat_check_portforward "$portforward_java_app" "$minikube_ip" "$java_app_nodeport"; then
+  ng
+  echo -n "port-foward for java-app ... "
+  check_listening $portforward_java_app && fail
+  socat_portforward "$portforward_java_app" "$minikube_ip" "$java_app_nodeport" || fail
+  ok
+else ok
+fi
+
 ### URL
 if google_cloudshell; then
   eval " $(grep WEB_HOST /etc/environment)"
@@ -324,13 +397,19 @@ if google_cloudshell; then
   echo "  ssh://$gitea_host:$gitea_nodeport_ssh"
   echo "  Username: $gitea_user, Password: $gitea_pass"
   echo "--------------------------------------------------------------------------------"
-  echo "Harbor WEB UI"
+  echo "Harbor WEB UI (browser)"
   echo "  https://$portforward_harbor-$WEB_HOST/"
   echo "  Username: $harbor_user, Password: $harbor_pass"
   echo
   echo "Harbor CLI (docker command)"
   echo "  http://$harbor_host:$harbor_nodeport"
   echo "  Username: $harbor_user, Password: $harbor_pass"
+  echo "--------------------------------------------------------------------------------"
+  echo "Java Web App"
+  echo "  https://$portforward_java_app-$WEB_HOST/"
+  echo
+  echo "Java Web App (curl, etc.)"
+  echo "  http://localhost:$portforward_java_app"
   echo "--------------------------------------------------------------------------------"
 else
   echo
@@ -349,7 +428,7 @@ else
   echo "  ssh://$gitea_host:$gitea_nodeport_ssh"
   echo "  Username: $gitea_user, Password: $gitea_pass"
   echo "--------------------------------------------------------------------------------"
-  echo "Harbor WEB UI"
+  echo "Harbor WEB UI (browser)"
   echo "  http://localhost:$portforward_harbor/"
   echo "  http://$harbor_host:$harbor_nodeport"
   echo "  Username: $harbor_user, Password: $harbor_pass"
@@ -357,5 +436,8 @@ else
   echo "Harbor CLI (docker command)"
   echo "  http://$harbor_host:$harbor_nodeport"
   echo "  Username: $harbor_user, Password: $harbor_pass"
+  echo "--------------------------------------------------------------------------------"
+  echo "Java Web App (curl, etc.)"
+  echo "  http://localhost:$portforward_java_app"
   echo "--------------------------------------------------------------------------------"
 fi
